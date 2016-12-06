9ac6be6740c16933af331f79605250b57d16c84e
MakeHui <makehuir@gmail.com>
2016-12-06 14:29:58 +0800

    add .DS_Store to .gitignore

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
+//
+//  main.cpp
+//  FileDBR
+//
+//  Created by MakeHui on 06/12/2016.
+//  Copyright © 2016 MakeHui. All rights reserved.
+//
+
 #include <iostream>
 
 #include "src/FdbrDatabase.h"
 #include "src/FdbrQueryBuilder.h"
 
-int main() {
-
-
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
     return 0;
 }
